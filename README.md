b3aa5c4f5f83b4b242f24cf3617cdbd6eff8527a
vishpandey <vishpandey2012@gmail.com>
2022-04-14 16:22:59 +0530

    change config

---
+++ README.md
@@ -0,0 +1,9 @@
+## PRATILIPI ASSIGNMENT
+
+Documentation Contains github link, deployement ip of the app and the deployment ip of API docs
+
+## Features
+
+- Github Link: [Github Link](https://github.com/vishpandey/ptlp_assignment.git) 
+- Deployment Ip: 20.219.84.13:5002
+- Api Docs - 20.219.84.13:5002/apidocs/index.html (Click on Default to get details of all apis)
--- config/config.py
+++ config/config.py
@@ -1,11 +1,11 @@
 class Config:
 	__conf = {
-	"KAFKA_SERVERS": ["20.219.84.36:9092"],
+	"KAFKA_SERVERS": ["52.140.57.176:9092"],
 	"REQUEST_MANAGER_TOPIC":"upload_request",
 	"KAFKA_CONSUMER_GROUP": "default-test-consumer",
 	"BULK_CONTENT_UPLOAD_TOPIC":"upload_new_content",
 	"BULK_USER_UPLOAD_TOPIC":"upload_new_user",
-	"MONGO_DB_URI": "20.219.84.37:27017",
+	"MONGO_DB_URI": "mongodb+srv://mongo2mongo:test123@cluster0.7ik1k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 	"MONGO_CONTENT_DB":"ptlp_content_db",
 	"MONGO_USER_DB": "ptlp_user_db",
 	"MONGO_CONTENT_COLLECTION":"content",
